97d942b81c67704506888e97a1c12ec1edb275d9
trevor-c <trevor.cunningham@uregina.ca>
2014-01-16 22:10:01 -0600

    updating lang strings to reference Kaltura CE

--- lang/en/repository_kaltura_uploader.php
+++ lang/en/repository_kaltura_uploader.php
@@ -1,11 +1,11 @@
 <?php
 
-$string['configplugin'] = 'Configuration for Kaltura uploader plug-in';
-$string['pluginname_help'] = 'Upload media to Kaltura';
-$string['pluginname'] = 'Upload media to Kaltura';
+$string['configplugin'] = 'Configuration for Kaltura CE uploader plug-in';
+$string['pluginname_help'] = 'Upload media to Kaltura CE';
+$string['pluginname'] = 'Upload media to Kaltura CE';
 
 // errors
-$string['upload_kaltura_error_process_upload'] = 'There was an error uploading the file to Kaltura';
+$string['upload_kaltura_error_process_upload'] = 'There was an error uploading the file to the Kaltura CE instance';
 
 // Capabilities
-$string['kaltura_uploader:view'] = 'View Kaltura uploader repo';
+$string['kaltura_uploader:view'] = 'View Kaltura CE uploader repo';
